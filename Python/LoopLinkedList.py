860e1fbb7c8333bf3d96aa92b0767e02ad53b4de
yhw-miracle <yhw_software@qq.com>
2019-05-22 18:22:48 +0800

    fix Python/LoopLinkedList.py

--- Python/LoopLinkedList.py
+++ Python/LoopLinkedList.py
@@ -5,3 +5,46 @@
 # @File: LoopLinkedList.py
 # @Software: PyCharm
 
+
+class LoopLinkedList(object):
+    """
+    循环链表
+    """
+    def __init__(self):
+        pass
+
+    def reverse(self):
+        """
+        单链表反转
+        :return:
+        """
+        pass
+
+    def is_cycle(self):
+        """
+        链表中环的检测
+        :return:
+        """
+        pass
+
+    def union(self):
+        """
+        两个有序链表合并
+        :return:
+        """
+        pass
+
+    def delete_by_back_index(self):
+        """
+        删除链表倒数第n个节点
+        :return:
+        """
+        pass
+
+    def middle_node(self):
+        """
+        求链表的中间节点
+        :return:
+        """
+        pass
+
