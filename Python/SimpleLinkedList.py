f4d93e7cbd02574963a01ac4773a8744f6b9d880
yhw-miracle <yhw_software@qq.com>
2019-05-22 18:04:29 +0800

    everyday_task/2019-05-22.md

--- Python/SimpleLinkedList.py
+++ Python/SimpleLinkedList.py
@@ -7,6 +7,13 @@
 
 
 class SimpleLinkedList(object):
+    """
+    1.单链表反转
+    2.链表中环的检测
+    3.两个有序链表合并
+    4.删除链表倒数第n个节点
+    5.求链表的中间节点
+    """
     def __init__(self, data, next_data = None):
         """
         初始化链表
