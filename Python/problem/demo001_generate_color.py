9d1dfb34fe6c800e10d6419f94b0c296dee1768e
yhw-miracle <yhw_software@qq.com>
2019-07-18 20:58:06 +0800

    write generate_color_way02()

--- Python/problem/demo001_generate_color.py
+++ Python/problem/demo001_generate_color.py
@@ -27,5 +27,47 @@ def generate_color_way01():
     return color_list
 
 
+def dec2hex(num):
+    """
+    十进制转换十六进制
+    16(D) ---> 10(H)
+        16/16=1......0
+        1/16=0......1
+    :param num:
+    :return:
+    """
+    base = [str(x) for x in range(10)] + [ chr(x) for x in range(ord('A'),ord('A')+6)]
+    hex_list = []
+    if num < 0:
+        return '-' + dec2hex(abs(num))
+    while True:
+        num,rem = divmod(num, 16)
+        hex_list.append(base[rem])
+        if num == 0:
+            return ''.join(hex_list[::-1])
+
+
+def generate_color_way02():
+    """
+    1. 生成`R`、`G`、`B`三组值，其值范围是`0~255`，十进制数;
+    2. 将十进制数转为十六进制数
+    时间复杂度为`O(n^3)`，但是在进行十进制数转换为十六进制数时，很耗时，故也是好的方法
+    :return:
+    """
+    rgb_dec_list = []
+    rgb_hex_list = []
+
+    for r in range(255):
+        for g in range(255):
+            for b in range(255):
+                print(r, g, b)
+                rgb_dec_list.append((r, g, b))
+                # color_hex_one = str(dec2hex(r)) + str(dec2hex(g)) + str(dec2hex(b))
+                # print(color_hex_one)
+                # rgb_hex_list.append(color_hex_one)
+
+    return rgb_hex_list
+
+
 if __name__ == '__main__':
     pass
