ea76c7eee58bf019903f8aab5b0d1e0d8af7cabc
yhw-miracle <yhw_software@qq.com>
2019-07-18 21:09:57 +0800

    demo001_search()

---
+++ Python/pure_handwriting/demo001_search.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time: 2019/7/18 21:06
+# @Author: yhw-miracle
+# @Email: yhw_software@qq.com
+# @File: demo001_search.py
+# @Software: PyCharm
+import random
+
+
+def single_search():
+    """
+    简单查询
+    :return:
+    """
+
+
+def binary_search():
+    """
+    二分查找
+    :return:
+    """
+
+def get_random_data_list():
+    """
+    随机测试数据
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    data_list = []
