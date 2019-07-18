41efa26184e8808cb31cff87ce18ce7fca9bd60b
yhw-miracle <yhw_software@qq.com>
2019-07-18 21:27:44 +0800

    test single_search(data_list, value)

--- Python/pure_handwriting/demo001_search.py
+++ Python/pure_handwriting/demo001_search.py
@@ -7,21 +7,6 @@
 import random
 
 
-def single_search(data_list, value):
-    """
-    简单查询
-    :param data_list: 查询数据列表，如：[1, 20, 23, 32, 21]
-    :param value: 查询值，如：32
-    :return: 查询结果，如，"data-list: 3 ---> 32"
-    """
-    i = 1
-    for v in data_list:
-        if v == value:
-            return "data_list: {} ---> {}".format(i, value)
-        i += 1
-    return "data_list no {}".format(value)
-
-
 def binary_search():
     """
     二分查找
@@ -41,6 +26,23 @@ def get_random_data_list(length):
     return data_list
 
 
+def single_search(data_list, value):
+    """
+    简单查询
+    :param data_list: 查询数据列表，如：[1, 20, 23, 32, 21]
+    :param value: 查询值，如：32
+    :return: 查询结果，如，"data-list: 3 ---> 32"
+    """
+    i = 1
+    for v in data_list:
+        if v == value:
+            return "data_list: {} ---> {}".format(i, value)
+        i += 1
+    return "data_list no {}".format(value)
+
+
 if __name__ == '__main__':
     data_list = get_random_data_list(30)
     print(data_list)
+    print(single_search(data_list, 23))
+    print(single_search(data_list, 12))
